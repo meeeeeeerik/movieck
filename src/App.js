f18cf54d236d12cf787188dd90261e0b11b728d9
Merik <merikreyimov@gmail.com>
2024-11-10 09:14:02 +0300

    api added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,24 @@
+import { useEffect } from 'react';
+import MovieApi from './api';
+
+const genreById = {
+  action: 28,
+  adventure: 12,
+  animation: 16,
+  comedy: 35,
+  horror: 27,
+  thriller: 53,
+  fantasy: 14,
+  history: 36,
+};
+
 function App() {
+  useEffect(() => {
+    MovieApi.search('Saw').then((response) =>
+      console.log(response)
+    );
+  }, []);
+
   return <div className="App">hi</div>;
 }
 
---
+++ src/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    accept: 'application/json',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWI0MWFmMmM5N2FiZmE3ZGRjMzk0OTI4ZWJiMWE0NyIsIm5iZiI6MTczMTEyMjA5MC4zNDY0MDkzLCJzdWIiOiI2NzJlNGFhYjJkNzY4MTMxZjlhNjYzZTciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.-zj5uWdwDy4e2GyDugBs5ADpbmm2t3djkqYEKUtgQRU',
+  },
+});
+
+class MovieApi {
+  static async getPopular() {
+    const response = await client.get('movie/popular?language=en-US&page=1');
+
+    return response.data.results;
+  }
+
+  static async getRated() {
+    const response = await client.get('movie/top_rated?language=en-US&page=1');
+
+    return response.data.results;
+  }
+
+  static async getUpcoming() {
+    const response = await client.get('movie/upcoming?language=en-US&page=1');
+
+    return response.data.results;
+  }
+
+  static async getMovie(id) {
+    const response = await client.get(`movie/${id}?language=en-US`);
+
+    return response.data;
+  }
+
+  static async getMovieByGenreId(genreId) {
+    const response = await client.get(
+      `discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=${genreId}`
+    );
+
+    return response.data.results;
+  }
+
+  static async search(query) {
+    const response = await client.get(
+      `search/movie?query=${query}&include_adult=false&language=en-US&page=1`
+    );
+
+    return response.data.results;
+  }
+}
+
+export default MovieApi;
